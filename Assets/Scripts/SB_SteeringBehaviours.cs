09eb8f5d1171235278dd8f9c69f7dd4d0b5fdfe6
tanya byrne <tanya.jo.byrne@gmail.com>
2016-04-02 12:30:57 +0100

    flee

--- Assets/Scripts/SB_SteeringBehaviours.cs
+++ Assets/Scripts/SB_SteeringBehaviours.cs
@@ -1,6 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+public enum AgentState
+{
+    seek,
+    flee
+}
+
 public class SB_SteeringBehaviours : MonoBehaviour
 {
 
@@ -16,41 +23,82 @@ public class SB_SteeringBehaviours : MonoBehaviour
         }
     }
 
+    public AgentState aiState;
+
+    public bool isFleeing = false;
+
 
     protected Vector3 desiredVelocity, _target;
     public float MaxSpeed, rotationSpeed;
     public GameObject targetObject;   
 
+
+    /// <summary>
+    /// determins seek vector
+    /// </summary>
+    /// <param name="seekTarget"></param>
+    /// <returns>seek direction vector</returns>
     public Vector3 Seek(Vector3 seekTarget)
     {
 
         desiredVelocity = seekTarget - transform.position;
         desiredVelocity.Normalize();
         desiredVelocity *= MaxSpeed;
-
         if (rb.velocity.magnitude > MaxSpeed)
             rb.velocity = rb.velocity.normalized * MaxSpeed;
-
-        //transform.forward = desiredVelocity;
         return desiredVelocity - (Vector3)rb.velocity;
 
     }
 
-    public void Face(Vector3 faceThis)
+
+    /// <summary>
+    /// rotates object to face its target
+    /// </summary>
+    /// <param name="faceThis"></param>
+    /// <param name="faceAway"></param>
+    public void Face(Vector3 faceThis, bool faceAway)
     {
         Vector3 dir = (faceThis - transform.position).normalized;
+        if (faceAway)
+            dir = -dir;
+
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
 
     }
-    
 
+    public Vector3 Flee(Vector3 fleeTarget)
+    {
+        desiredVelocity = transform.position - fleeTarget;
+        desiredVelocity.Normalize();
+        desiredVelocity *= MaxSpeed;
+        if (rb.velocity.magnitude > MaxSpeed)
+            rb.velocity = rb.velocity.normalized * MaxSpeed;
+        return desiredVelocity - (Vector3)rb.velocity;
+
+    }
     
 
     public void FixedUpdate()
     {
         _target = targetObject.transform.position;
-        rb.AddForce(Seek(_target));
+
+        switch (aiState)
+        {
+            default:
+            case AgentState.seek:
+                isFleeing = false;
+                rb.AddForce(Seek(_target));
+                break;
+            case AgentState.flee:
+                isFleeing = true;
+                rb.AddForce(Flee(_target));
+                break;
+                
+
+
+        }
+        
     }
 
 
@@ -60,7 +108,7 @@ public class SB_SteeringBehaviours : MonoBehaviour
     public void Update()
     {
 
-        Face(_target);
+        Face(_target, isFleeing);
 
     }
 }
